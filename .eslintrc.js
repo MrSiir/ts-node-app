71d893b4c86e11c7458519a49395113368f0c435
Xavier Ferrón <javi@iquadrat.com>
2023-04-22 18:55:20 +0200

    🎉 feat: add initial project files and configurations

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    es2018: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 2018,
+    project: 'tsconfig.json'
+  },
+  rules: {}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# ts-node-app
+
+Basic template for typescript and ts-node
+
+```
+> yarn
+> yarn start
+```
